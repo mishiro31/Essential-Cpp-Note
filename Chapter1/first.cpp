3afb0c5bf8b74e2a203601e6b7f84ee0f845f8f6
Misro <1204295557@qq.com>
2024-09-26 22:50:25 +0800

    first code

---
+++ Chapter1/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string userName;
+    cout << "Please enter your first name: ";
+    cin >> userName;
+    cout << "\nHello, " << userName << " ... and goodbye!\n";
+    return 0;
+}
